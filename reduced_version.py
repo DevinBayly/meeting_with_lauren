2ef60dffcf2bf9945e556205b63a7d8ac4780ffe
Devin Bayly <baylyd@email.arizona.edu>
2024-02-14 22:37:02 +0000

    showing how sections works via config editing

--- reduced_version.py
+++ reduced_version.py
@@ -3,10 +3,12 @@ import streamlit as st
 
 class CreateCircle(ThreeDScene):
     def construct(self):
+        self.next_section("first section")
         axes = ThreeDAxes()
         circle = Circle()
         self.add(circle)
         self.wait(1)
+        self.next_section("second section")
         self.add(axes)
         self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
         cylinder = Cylinder()
@@ -14,7 +16,7 @@ class CreateCircle(ThreeDScene):
         self.wait()
 
 st.title('Bare bones')
-
+config.save_sections = True
 c = CreateCircle()
 c.render()
 st.video("media/videos/1080p60/CreateCircle.mp4")
