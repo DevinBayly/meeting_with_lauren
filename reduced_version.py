9d036eec073dbea3f114d5213281110d7ec82aa6
Devin Bayly <baylyd@email.arizona.edu>
2024-02-20 17:45:26 +0000

    adding example of making a set of lines

--- reduced_version.py
+++ reduced_version.py
@@ -1,22 +1,24 @@
 from manim import *
 import streamlit as st
+import numpy as np
 
-class CreateCircle(ThreeDScene):
+class CreateCircle(Scene):
     def construct(self):
-        self.next_section("first section")
-        axes = ThreeDAxes()
-        circle = Circle()
-        self.add(circle)
-        self.wait(1)
-        self.next_section("second section")
-        self.add(axes)
-        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
-        cylinder = Cylinder()
-        self.play(Transform(circle,cylinder))
-        self.wait()
+        rand_xys = np.random.random((5,2)).tolist()
+        print(rand_xys)
+        # range of theta is also an option
+        for i in range(0,len(rand_xys)):
+            rand_xy = rand_xys[i]
+            line = Line(start=(0,0,0),end=(rand_xy[0],rand_xy[1],0))
+            self.add(line)
 
-st.title('Bare bones')
-config.save_sections = True
+        # self.next_section("first section")
+        
+        
+
+# st.title('Bare bones')
+# config.save_sections = True
+# config.renderer = "opengl"
 c = CreateCircle()
 c.render()
-st.video("media/videos/1080p60/CreateCircle.mp4")
+# st.video("media/videos/1080p60/CreateCircle.mp4")
