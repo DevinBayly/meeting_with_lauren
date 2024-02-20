2b004e12df16cce6c934bead3bc68eecb12b9226
Devin Bayly <baylyd@email.arizona.edu>
2024-02-20 17:54:14 +0000

    got the file to show lines generated one at a time, had to use command line

--- reduced_version.py
+++ reduced_version.py
@@ -6,14 +6,25 @@ class CreateCircle(Scene):
     def construct(self):
         rand_xys = np.random.random((5,2)).tolist()
         print(rand_xys)
-        # range of theta is also an option
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+        # # range of theta is also an option
         for i in range(0,len(rand_xys)):
             rand_xy = rand_xys[i]
             line = Line(start=(0,0,0),end=(rand_xy[0],rand_xy[1],0))
+            self.wait()
             self.add(line)
 
         # self.next_section("first section")
         
+
         
 
 # st.title('Bare bones')
