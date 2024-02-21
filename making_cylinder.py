502fa4e7632f94efe1f7889251cf52d4d29ad55d
Devin Bayly <baylyd@email.arizona.edu>
2024-02-21 22:20:27 +0000

    got the animations to show again after pycache removal

--- making_cylinder.py
+++ making_cylinder.py
@@ -2,23 +2,25 @@ from manim import *
 import streamlit as st
 import numpy as np
 
-class MyCylinder(ThreeDScene):
+class AnotherTest(ThreeDScene):
     def construct(self):
         axes = ThreeDAxes()
-        cylinder = Cylinder(fill_color=RED,radius=2, height=3)
+        cylinder = Cylinder(fill_color=RED,radius=8, height=3)
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.wait()
         self.add(axes, cylinder)
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen 
 
         # self.next_section("first section")
         
 
         
 
+
 st.title('Bare bones')
 # config.save_sections = True
 # config.renderer = "opengl"
-c = MyCylinder()
-
+c = AnotherTest()
 c.render()
-st.video("media/videos/1080p60/MyCylinder.mp4")
+st.video("media/videos/1080p60/AnotherTest.mp4")
