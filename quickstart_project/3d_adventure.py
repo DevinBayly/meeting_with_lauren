20582118bee7531964f4db95d46a078e1028066b
devinbayly <baylyd@arizona.edu>
2024-03-13 10:38:12 -0700

    start working on local system

--- quickstart_project/3d_adventure.py
+++ quickstart_project/3d_adventure.py
@@ -1,22 +1,21 @@
 from manim import *
-import streamlit as st
 import numpy as np 
 
 class CreateCircle1(ThreeDScene):
     def construct(self):
 
         # self.next_section("first section")
-        # axes = ThreeDAxes()
-        # circle = Circle()
-        # slide = st.slider("pick a number!",1,10)
-        # circle.set_fill(PINK, opacity=0.5)
-        # line = Line()
+        axes = ThreeDAxes()
+        circle = Circle()
+        slide =2
+        circle.set_fill(PINK, opacity=0.5)
+        line = Line()
 
-        # self.add(line)
-        # self.wait(1)
-        # self.play(Rotate(Line(), angle=2*PI, about_point= LEFT, rate_func=linear))
-        # self.add(circle)
-        # self.wait(1)
+        #self.add(line)
+        #self.wait(1)
+        #self.play(Rotate(Line(), angle=2*PI, about_point= LEFT, rate_func=linear))
+        #self.add(circle)
+        #self.wait(1)
 
         # self.next_section("second section")
         # self.add(axes)
@@ -37,7 +36,7 @@ class CreateCircle1(ThreeDScene):
             x = np.cos(theta)
             y=np.sin(theta)
             line = Line(start=(0,0,0), end=(x,y,0))
-            self.wait(.01)
+            self.wait(.1)
             self.add(line)
 
         circle = Circle()
@@ -60,9 +59,7 @@ class CreateCircle1(ThreeDScene):
         #     self.wait(.1)
 
 
-st.title('Bare bones')
 
 c = CreateCircle1()
-config.save_sections=True  
+config.max_files_cached=800
 c.render()
-st.video("media/videos/1080p60/CreateCircle1.mp4")
