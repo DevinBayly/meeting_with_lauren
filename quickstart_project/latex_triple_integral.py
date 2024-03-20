f7ad2ea02796bfe82911240b0365d067fce261f4
Devin Bayly <baylyd@email.arizona.edu>
2024-03-20 19:53:00 +0000

    demonstration of how to add in the triple integral with latex

--- quickstart_project/latex_triple_integral.py
+++ quickstart_project/latex_triple_integral.py
@@ -3,10 +3,20 @@ import streamlit as st
 
 class HelloLaTeX(Scene):
     def construct(self):
-        tex = Tex(r"Hello \LaTeX ", font_size=144)
+        tex = Tex(r"$\int$",r"$\int$",r"$\int$", font_size=144)
         self.add(tex)
+        labels = index_labels(tex)
+        self.add(labels)
         self.wait(3)
+        tex[0].set_color(RED)
+        self.wait()
+        tex[1].set_color(RED)
+        self.wait()
+        tex[2].set_color(RED)
+        self.wait(3)
+        self.remove(labels)
         self.remove(tex)
+        self.wait()
         tex2 = Tex(r"goodbye \LaTeX ", font_size=144)
         self.add(tex2)
         self.wait(3)
@@ -15,3 +25,5 @@ class HelloLaTeX(Scene):
 h = HelloLaTeX()
 h.render()
 st.video("media/videos/1080p60/HelloLaTeX.mp4")
+
+config.max_files_cached=800
