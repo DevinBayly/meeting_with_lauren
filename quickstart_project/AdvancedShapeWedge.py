1d2773d3f0c5c9675ad83ddd7f431cfe35648347
devinbayly <baylyd@arizona.edu>
2024-04-22 21:25:51 -0700

    start using axes coordinates, make color changes

--- quickstart_project/AdvancedShapeWedge.py
+++ quickstart_project/AdvancedShapeWedge.py
@@ -8,50 +8,96 @@ class Wedge(ThreeDScene):
         # note that we can create radius and re-use it with all the parts of this
         # TODO set this up so that a range of the 0-2pi space can be represented at each step
         radius = 2
-        shape_color = GREEN
+        self.shape_color = GREEN
         axes = ThreeDAxes()
+        self.axes = axes
         self.add(axes)
         # TODO figure out how to move camera to the right location
         self.move_camera(phi=70 * DEGREES, theta=30 * DEGREES)
         print("addign in lines to fill 2d circle")
+        # add in the ability to turn the poly slices and lines off or modify, these are lists that we will update later
+        self.slice_polys = []
+        self.lines = []
+        poly_points=[]
         # making the polygon for the formula -y+5
         # coordinates counter clockwise are xyz 0,0,5 5,0,5 5,5,0 0,5,0 this is the
-        poly_points=[]
-        poly_points.append((0,0,1))
-        poly_points.append((1,0,1))
-        poly_points.append((1,1,0))
-        poly_points.append((0,1,0))
+        poly_points.append(self.axes.c2p(0,0,1))
+        poly_points.append(self.axes.c2p(1,0,1))
+        poly_points.append(self.axes.c2p(1,1,0))
+        poly_points.append(self.axes.c2p(0,1,0))
         p = Polygon(*poly_points)
         p.set_fill(opacity=1)
-        self.add(p)
+        self.add_foreground_mobjects(p)
         self.wait(2)
-        self.remove(p)
+        self.move_camera(phi=70 * DEGREES, theta=30 * DEGREES,zoom=2)
+        # self.remove(p)
+        # TODO make the fill gradual and animated
+        p.set_fill(opacity=0)
+        top_z = 1
+        number_of_planes =4
+        z_delta = top_z/number_of_planes
+        sections = 5
+        animate_drawing = True
+        # TODO make the color of the segments only for the first pass through the plane
+        # then for the other ones leave it alone
+        for np in range(number_of_planes):
+            z = top_z - z_delta*np
+            y= -z + 1
+            self.wait(.08)
+            self.make_wedge_piece(sections,z,y,animate=animate_drawing)
+            if np ==0:
+                # do additional refine step
+                sections=15
+                animate_drawing = False
+                self.make_wedge_piece(sections,z,y,animate=animate_drawing)
+                self.wait(1)
+        # TODO make the stroke color go away for all the drawn polygons
+        self.make_solid()
         self.wait()
+        number_of_planes=20
+        z_delta = top_z/number_of_planes
+        for np in range(number_of_planes):
+            z = top_z - z_delta*np
+            y= -z + 1
+            # self.wait(.08)
+            self.make_wedge_piece(sections,z,y,animate=animate_drawing,plane_wait=.08,stroke_color=self.shape_color)
+            # self.wait(1)
+        self.wait(2)
+
 
         # start with drawing a vertical line, then a few polygons that extend from the z axis to the largest values of x
-    def make_wedge_piece(self,pieces,top,yoffset):
-        first_line = Line(start=poly_points[0],end=poly_points[1])
+    def make_wedge_piece(self,pieces,top,yoffset,animate=True,plane_wait = .8,stroke_color=WHITE):
+        first_line = Line(start=self.axes.c2p(0,yoffset,top),end=self.axes.c2p(1,yoffset,top))
         self.add(first_line)
-        self.wait(2)
+        self.wait(plane_wait)
+        self.lines.append(first_line)
         # pieces = 5
+        # TODO make it so that it's optional to see the rectangles animate out
         for l in range(pieces):
             delta = 1/pieces
-            s = (delta*l,0,1)
+            # the rectangles that are drawn descendin from the line and iterating out from the x axis
+            s = (delta*l,yoffset,top)
+            # 
             s_lower = (*s[:2],0)
-            e = (delta*l+delta,0,1)
+            e = (delta*l+delta,yoffset,top)
             e_lower = (*e[:2],0)
             poly_points=[]
-            poly_points.append(s)
-            poly_points.append(s_lower)
-            poly_points.append(e_lower)
-            poly_points.append(e)
-            p = Polygon(*poly_points,stroke_color=WHITE)
-            p.set_fill(opacity=1,color=shape_color)
+            poly_points.append(self.axes.c2p(*s))
+            poly_points.append(self.axes.c2p(*s_lower))
+            poly_points.append(self.axes.c2p(*e_lower))
+            poly_points.append(self.axes.c2p(*e))
+            p = Polygon(*poly_points,stroke_color=stroke_color)
+            p.set_fill(opacity=1,color=self.shape_color)
             self.add(p)
-            self.wait()
+            self.slice_polys.append(p)
+            if animate:
+                self.wait(.08)
 
         # then move to a new line and redo the process
-
+    def make_solid(self):
+        # take the polygons that have a certain stroke color and turn that off
+        for p in self.slice_polys:
+            p.set_stroke(self.shape_color)
         
 
 
