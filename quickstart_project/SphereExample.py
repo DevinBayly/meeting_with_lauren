stuck at final formula step, equations appearing in wrong locations

quickstart_project/SphereExample.py
@@ -99,28 +99,31 @@ using integration""")
 #        self.add(fmla_6)
 #        self.remove(fmla_5)
 #        self.wait(2)
+        # TODO ask lauren if this is the right time to be pausing in the animation so that we can draw the axes and stuff
         upper_right_fmla = Tex(
-r"$\int_{0}^{2\pi}\int_{0}^{\pi}\frac{8}{3} sin(\phi)\, d\phi\, d\theta$ \\ "
+r"$\int_{0}^{2\pi}$",r"$\int_{0}^{\pi}\frac{8}{3}sin(\phi)\, d\phi\,$",r"$ d\theta$ \\ "
             ,font_size = 70)
         upper_right_fmla.to_edge(UR)
         # self.remove(fmla_6)
-        self.add(upper_right_fmla)
+        self.add_fixed_in_frame_mobjects(upper_right_fmla)
         self.wait(2)
         self.add(axes)
-        # add in the axes and draw a first line of the spoke that come down the rho
+        ## add in the axes and draw a first line of the spoke that come down the rho
 
 
-        #self.begin_ambient_camera_rotation(rate=-.2)
+        ##self.begin_ambient_camera_rotation(rate=-.2)
 
         self.move_camera(phi=70 * DEGREES, theta=30 * DEGREES)
-        #print("addign in lines to fill 2d circle")
+        ##print("addign in lines to fill 2d circle")
+        #shape_color=ORANGE
+        #upper_right_fmla[1].set_color(shape_color)
         #sections = 10
         #lines=[]
-        #angle_start = 0
-        #angle_range = np.pi
+        angle_start = 0
+        angle_range = np.pi
         ## set up inner and outer radii
-        #inner_radii =0
-        #outer_radii = radius
+        inner_radii =0
+        outer_radii = radius
         #for i in range(0,sections+1):
         #    theta = angle_start+ angle_range/sections*i
         #    z_start=inner_radii* np.cos(theta)
@@ -133,16 +136,74 @@ r"$\int_{0}^{2\pi}\int_{0}^{\pi}\frac{8}{3} sin(\phi)\, d\phi\, d\theta$ \\ "
         #    self.add(line)
         #    lines.append(line)
         ## replace circles with arcs
-        #first_disc = AnnularSector(inner_radius= inner_radii,outer_radius = outer_radii,start_angle = angle_start,angle=angle_start+angle_range )
-        #first_disc.set_fill(shape_color, opacity=1.0)
-        #first_disc.set_stroke(color=WHITE, width=1)
-        #first_disc.rotate(PI/2,about_point=ORIGIN,axis=np.array([0,1,0]))
-        #self.add(first_disc)
-        ### want to try to rotate the discs for sphereical
-        #self.wait(2)
-        #print(first_disc.get_bottom())
-        ##first_disc.rotate(90,axis=np.array([1,0,0]))
-        #self.wait(2)
+        first_disc = AnnularSector(inner_radius= inner_radii,outer_radius = outer_radii,start_angle = angle_start,angle=angle_start+angle_range )
+        first_disc.set_fill(shape_color, opacity=1.0)
+        first_disc.set_stroke(color=WHITE, width=1)
+        first_disc.rotate(PI/2,about_point=ORIGIN,axis=np.array([0,1,0]))
+        self.add(first_disc)
+        ## want to try to rotate the discs for sphereical
+        self.wait(2)
+        print(first_disc.get_bottom())
+        #first_disc.rotate(90,axis=np.array([1,0,0]))
+        # TODO think about the timing on this, is it too long?
+        self.wait(2)#
+        self.remove(axes)
+        self.remove(first_disc)
+
+
+
+        # now we need to go back into the integration to do the next couple steps of this 
+        fmla_6 = Tex(
+r"$\int_{0}^{2\pi}$",r"$\int_{0}^{\pi}\frac{8}{3}sin(\phi)\, d\phi\,$",r"$ d\theta$ \\ "
+            ,font_size = 70)
+        fmla_6.to_edge(UP)
+        self.play(Transform(upper_right_fmla,fmla_6))
+        self.add_fixed_in_frame_mobjects(fmla_6)
+        self.remove(upper_right_fmla)
+        self.wait(2)
+
+        self.move_camera(theta = None,phi=None)
+        # making the integrations down the page now
+        fmla_7 = Tex(
+r"$\int_{0}^{2\pi}$",r"$\int_{0}^{\pi}\frac{8}{3}sin(\phi)\, d\phi\,$",r"$ d\theta$ \\ ",
+r"$\frac{8}{3}\int_{0}^{2\pi}-cos(\phi)\rvert_{0}^{\pi}\, d\theta$ \\"
+            ,font_size = 70)
+        fmla_7.to_edge(UP)
+        self.play(Transform(fmla_7,fmla_6))
+        self.add_fixed_in_frame_mobjects(fmla_7)
+        self.remove(fmla_6)
+        self.wait(2)
+
+        fmla_8 = Tex(
+r"$\int_{0}^{2\pi}$",r"$\int_{0}^{\pi}\frac{8}{3}sin(\phi)\, d\phi\,$",r"$ d\theta$ \\ ",
+r"$\frac{8}{3}\int_{0}^{2\pi}-cos(\phi)\rvert_{0}^{\pi}\, d\theta$ \\",
+r"$\frac{8}{3}\int_{0}^{2\pi}-(0) + (1) d\theta$ \\"
+            ,font_size = 70)
+        fmla_8.to_edge(UP)
+        self.play(Transform(fmla_7,fmla_8))
+        self.add_fixed_in_frame_mobjects(fmla_8)
+        self.remove(fmla_7)
+        self.wait(2)
+
+
+
+        fmla_9 = Tex(
+r"$\int_{0}^{2\pi}$",r"$\int_{0}^{\pi}\frac{8}{3}sin(\phi)\, d\phi\,$",r"$ d\theta$ \\ ",
+r"$\frac{8}{3}\int_{0}^{2\pi}-cos(\phi)\rvert_{0}^{\pi}\, d\theta$ \\",
+r"$\frac{8}{3}\int_{0}^{2\pi}-(0) + (1) d\theta$ \\"
+            ,font_size = 70)
+        fmla_9.to_edge(UP)
+        self.play(Transform(fmla_9,fmla_8))
+        self.add_fixed_in_frame_mobjects(fmla_9)
+        self.remove(fmla_8)
+        self.wait(2)
+#  
+#  
+# $\frac{8}{3}\int_{0}^{2\pi}d\theta$ \\ 
+
+
+
+
         #ds =[]
         ## TODO figure out how to not draw over the earlier ones with the last ones
         ## TODO think about repositioning the camera to prevent the overdrawing , either through explicit coords or ambient (lauren's suggestion)