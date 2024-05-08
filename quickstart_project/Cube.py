3f92c22e91c68bcf69481b5db353b5cc4472cc31
laur3nfost3r <156472413+laur3nfost3r@users.noreply.github.com>
2024-05-08 22:28:01 +0000

    final cube woo

--- quickstart_project/Cube.py
+++ quickstart_project/Cube.py
@@ -55,7 +55,7 @@ class CubeExample(ThreeDScene):
         self.wait(2)
 
 
-        fmla_2 = Tex(r"$\int_{0}^{2}$",r"$\int_{0}^{2}$",r"$x\rvert_{0}^{2}$",r"$dydz$", font_size=100)
+        fmla_2 = Tex(r"$\int_{0}^{2}$",r"$\int_{0}^{2}$",r"$x\rvert_{0}^{2}$",r"$dydz$", font_size=85)
         fmla_2.next_to(fmla_1,DOWN)
         self.add_fixed_in_frame_mobjects(fmla_2)
         #self.play(Write(fmla_2))
@@ -64,7 +64,7 @@ class CubeExample(ThreeDScene):
         # self.add_fixed_in_frame_mobjects(fmla_2)
         # self.wait(2)
 
-        fmla_3 = Tex(r"$\int_{0}^{2}$",r"$\int_{0}^{2}$", r"2",r"$dy$",r"$dz$", font_size=100)
+        fmla_3 = Tex(r"$\int_{0}^{2}$",r"$\int_{0}^{2}$", r"2",r"$dy$",r"$dz$", font_size=85)
         fmla_3.next_to(fmla_2,DOWN)
         self.add_fixed_in_frame_mobjects(fmla_3)
         fmla_3[2].set_color(GREEN)
@@ -111,13 +111,13 @@ class CubeExample(ThreeDScene):
 
         # FRAME FIVE
 
-        fmla_5 = Tex(r"$\int_{0}^{2}$",r"$2y\rvert_{0}^{2}$",r"$dz$", font_size=100)
+        fmla_5 = Tex(r"$\int_{0}^{2}$",r"$2y\rvert_{0}^{2}$",r"$dz$", font_size=85)
         self.add_fixed_in_frame_mobjects(fmla_5)
         fmla_5[1].set_color(ORANGE)
         fmla_5.next_to(fmla_3,DOWN)
         self.wait(2)
 
-        fmla_6 = Tex(r"$\int_{0}^{2}$",r"4",r"$dz$", font_size=100)
+        fmla_6 = Tex(r"$\int_{0}^{2}$",r"4",r"$dz$", font_size=85)
         self.add_fixed_in_frame_mobjects(fmla_6)
         fmla_6[1].set_color(ORANGE)
         fmla_6.next_to(fmla_5,DOWN)
@@ -125,9 +125,9 @@ class CubeExample(ThreeDScene):
         self.remove(fmla_3,fmla_5)
 
         fmla_6.generate_target()
-        fmla_6.target.shift(UP*5,RIGHT*2).scale(.6)
+        fmla_6.target.shift(UP*4,RIGHT*2).scale(.7)
         self.play(MoveToTarget(fmla_6))
-        self.wait(2)
+        self.wait(1)
 
         self.move_camera(phi=70 * DEGREES, theta=45 * DEGREES,frame_center=axes.c2p(-2,0,0))
 
@@ -141,7 +141,7 @@ class CubeExample(ThreeDScene):
         self.wait(2)
 
 
-        slices = 20
+        slices = 13
         side = 2
         squares = []
         shape_color = PINK
@@ -156,30 +156,46 @@ class CubeExample(ThreeDScene):
 
         #FRAME SEVEN
 
-        fmla_7 = Tex(r"$4\rvert_{0}^{2}$", font_size=100)
+        fmla_7 = Tex(r"$4\rvert_{0}^{2}$", font_size=85)
         self.add_fixed_in_frame_mobjects(fmla_7)
         fmla_7[0].set_color(PINK)
         fmla_7.next_to(fmla_6,DOWN)
         self.wait(2)
 
-        fmla_8 = Tex(r"$(4(2)-4(0))$", font_size=100)
+        fmla_8 = Tex(r"$(4(2)-4(0))$", font_size=85)
         self.add_fixed_in_frame_mobjects(fmla_8)
         fmla_8[0].set_color(PINK)
         fmla_8.next_to(fmla_7,DOWN)
         self.wait(2)
 
-        fmla_9 = Tex("Volume = 8", font_size=100)
+        fmla_9 = Tex("Volume = 8", font_size=85)
         self.add_fixed_in_frame_mobjects(fmla_9)
         fmla_9[0].set_color(BLUE)
         fmla_9.next_to(fmla_8,DOWN)
         self.wait(0.5)
 
-        prism = Prism(dimensions=axes.c2p(2,2,2))
-        self.add_fixed_in_frame_mobjects(prism)
-        self.wait(2)
 
-
-
+        vertex_coords = [
+            axes.c2p(0, 0, 0),                                                              axes.c2p(2, 0, 0),  
+            axes.c2p(2, 2, 0),  
+            axes.c2p(0, 2, 0),  
+            axes.c2p(0, 0, 2),  
+            axes.c2p(2, 0, 2),  
+            axes.c2p(2, 2, 2),  
+            axes.c2p(0, 2, 2),  
+        ]
+        faces_list = [
+            [0, 1, 2, 3],
+            [4, 5, 6, 7],
+            [0, 1, 5, 4],
+            [2, 3, 7, 6],
+            [1, 2, 6, 5],
+            [0, 3, 7, 4],
+        ]
+        fnl_cube = Polyhedron(vertex_coords, faces_list)
+        fnl_cube.set_fill(opacity=1)
+        self.add(fnl_cube)
+        self.wait(3)
 
 
 
@@ -187,150 +203,3 @@ c = CubeExample()
 c.render()
 st.video("media/videos/480p15/CubeExample.mp4")
 
-
-
-
-
-
-
-# #         #fmla_1[2].set_color(shape_color)
-# #         #fmla_1[3].set_color(shape_color)
-# #         #fmla_1.to_corner(UR)
-# #         #self.add_fixed_in_frame_mobjects(fmla_1)
-# #         #self.wait(2)
-# #         #self.remove(fmla_1)
-# #         ## second step of the formula development
-# #         #fmla_2 = Tex(r"$\int_{0}^{5}$",r"$\int_{0}^{2\pi}$",r"$\frac{1}{2}r^{2}  \vert_{0}^{2}$",r" $d\theta dz$", font_size=70)
-# #         #fmla_2[2].set_color(shape_color)
-# #         #fmla_2.move_to(fmla_1)
-# #         #self.add_fixed_in_frame_mobjects(fmla_2)
-# #         #self.wait(2)
-# #         #self.remove(fmla_2)
-# #         ## third step of the formula dev
-# #         #fmla_3 = Tex(r"$\int_{0}^{5}$",r"$\int_{0}^{2\pi}$",r"$\frac{1}{2}(2)^{2} - \frac{1}{2}(0)^{2}$",r" $d\theta dz$", font_size=50)
-# #         #fmla_3[2].set_color(shape_color)
-# #         #fmla_3.move_to(fmla_1)
-# #         #self.add_fixed_in_frame_mobjects(fmla_3)
-# #         #self.wait(2)
-# #         #self.remove(fmla_3)
-# #         ## last formula step for this section
-# #         #fmla_4 = Tex(r"$\int_{0}^{5}$",r"$\int_{0}^{2\pi}$",r"2",r" $d\theta dz$", font_size=70)
-# #         #fmla_4[2].set_color(shape_color)
-# #         #fmla_4.move_to(fmla_1)
-# #         #self.add_fixed_in_frame_mobjects(fmla_4)
-# #         #self.wait(2)
-# #         #self.remove(fmla_4)
-# #         # TODO remove the single line generated before that we had with all the equations
-# #         # next part of the animation
-# #         shape_color = BLUE
-# #         fmla_5 = Tex(r"$\int_{0}^{5}$",r"$\int_{0}^{2\pi} 2 d\theta$",r"dz", font_size=70)
-# #         fmla_5[1].set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_5.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_5)
-# #         # self.wait(2)
-
-
-# #         # TODO consider how many lines we want to draw per quadrant, not sections total perhaps, with not enough sections this seems very sparse
-# #         for i in range(0,sections+1):
-# #             # TODO make the values use the axes c2p
-# #             theta = angle_start+ angle_range/sections*i
-# #             x_start=inner_radii* np.cos(theta)
-# #             y_start=inner_radii* np.sin(theta)
-# #             x_end=outer_radii* np.cos(theta)
-# #             y_end=outer_radii* np.sin(theta)
-# #             line = Line(start=(x_start,y_start,0), end=(x_end,y_end,0),color=shape_color)
-# #             # TODO put this back in for aniimated spokes
-# #             # self.wait(.08)
-# #             # self.play(Create(line))
-# #             self.add(line)
-# #             lines.append(line)
-# #         self.wait(2)
-# #         self.remove(fmla_5)
-# #         fmla_6 = Tex(r"$\int_{0}^{5}$",r"$ 2\theta \rvert_{0}^{2\pi}$",r"dz", font_size=70)
-# #         fmla_6[1].set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_6.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_6)
-# #         self.wait(2)
-# #         self.remove(fmla_6)
-# #         fmla_7 = Tex(r"$\int_{0}^{5}$",r"$ 2(2\pi) - 2(0)$",r"dz", font_size=70)
-# #         fmla_7[1].set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_7.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_7)
-# #         self.wait(2)
-# #         self.remove(fmla_7)
-# #         # replace circles with arcs
-# #         fmla_8 = Tex(r"$\int_{0}^{5}$",r"$ 4\pi $",r"dz", font_size=70)
-# #         fmla_8[1].set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_8.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_8)
-# #         self.wait(2)
-# #         self.remove(fmla_8)
-# #         first_disc = AnnularSector(inner_radius= inner_radii,outer_radius = outer_radii,start_angle = angle_start,angle=angle_start+angle_range )
-# #         first_disc.set_fill(shape_color, opacity=1.0)
-# #         first_disc.set_stroke(color=WHITE, width=1)
-# #         self.add(first_disc)
-# #         self.wait(2)
-# #         shape_color = PINK
-# #         first_disc.set_fill(shape_color,opacity = 1.0)
-# #         self.move_camera(phi=70 * DEGREES, theta=30 * DEGREES)
-# #         for line in lines:
-# #             self.remove(line)
-
-# #         # self.remove(first_disc)
-# #         slices = 12
-# #         discs = []
-# #         # last set of fmlas
-# #         self.remove(fmla_8)
-# #         # replace circles with arcs
-# #         fmla_9 = Tex(r"$\int_{0}^{5} 4\pi $",r"dz", font_size=70)
-# #         fmla_9.set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_9.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_9)
-# #         self.wait(2)
-# #         for i in range(0,slices+1):
-
-# #             disc =  AnnularSector(inner_radius= inner_radii,outer_radius = outer_radii,start_angle = angle_start,angle=angle_start+angle_range ).shift(i*0.1*OUT)
-# #             discs.append(disc)
-# #             disc.set_fill(shape_color, opacity=1.0)
-# #             disc.set_stroke(color=WHITE, width=1)
-# #             self.add(disc)
-# #             # note we can't go belo .066 because our default frame rate is 1/15 because we are doing 15fps ffmpeg
-# #             self.wait(.068)
-# # # 
-# #         self.remove(fmla_9)
-# #         # replace circles with arcs
-# #         # TODO consider drawing all these "in back, so that the axes aren't covered?"
-# #         fmla_9 = Tex(r"$ 4\pi \rvert_{0}^{5}$", font_size=70)
-# #         fmla_9.set_color(shape_color)
-# #         # very helpful positioning option
-# #         fmla_9.to_edge(UR)
-# #         self.add_fixed_in_frame_mobjects(fmla_9)
-# #         self.wait(2)
-# #         # attempt to make a cylinder that has it's bottom at the first circle's position, and it's top at the top of the last disc
-# #         #for x in discs:
-# #         #    print(x.get_start(),x.get_end(),x.get_center())
-# #         ## create a cylinder that we are going to overlay on the shape
-# #         ## get the last disc
-# #         ## and get it's 3 component, the y height
-# #         self.remove(first_disc)
-# #         self.remove(fmla_9)
-# #         fmla_10 = Tex("Volume  = ",r"$20\pi$",font_size = 70)
-# #         fmla_10.to_edge(UR)
-# #         fmla_10.set_color(shape_color)
-# #         self.add_fixed_in_frame_mobjects(fmla_10)
-# #         self.wait(2)
-# #         discs_height = discs[-1].get_center()[2]
-# #         cyl = Cylinder(height = discs_height,radius=radius,v_range=[angle_start,angle_start+angle_range], checkerboard_colors=[shape_color,shape_color],fill_color=shape_color,resolution=(6,6),show_ends=False)
-# #         cyl.align_to(discs[-1],OUT)
-# #         self.add(cyl)
-# #         self.wait(1)
-# #         # stop short of the last one so that there's a cap
-# #         for disc in discs[:-1]:
-# #             self.remove(disc)
-# #         self.wait(1)
-# #         print("finished removing")
